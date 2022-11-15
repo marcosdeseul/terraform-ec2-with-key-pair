ca8b4ab2d85165896e11bafe2e5a095276458de6
Marco Kwak <marcosdeseul@gmail.com>
2022-11-15 15:00:40 +0800

    Remove CentOS

--- locals.tf
+++ locals.tf
@@ -1,4 +1,3 @@
 locals {
-  ami_map = { ubuntu: data.aws_ami.ubuntu.id, centos: data.aws_ami.centos.id }
-  ami = var.ami == "" ? local.ami_map[var.os] : var.ami
+  ami = var.ami == "" ? data.aws_ami.ubuntu.id : var.ami
 }
